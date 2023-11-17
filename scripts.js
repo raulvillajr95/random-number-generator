91f47080c52be894d69a2ced37c7c7d9366f4b2c
Raul <raulvillajr95@gmail.com>
2023-11-16 22:10:21 -0800

    add css alignment

---
+++ scripts.js
@@ -0,0 +1,26 @@
+function generateRandomNum(num1, num2) {
+  return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+}
+
+let generateBtn = document.getElementById('generate');
+let num1Text = document.getElementById('num1');
+let num2Text = document.getElementById('num2');
+let resultElem = document.getElementById('value');
+
+generateBtn.addEventListener('click', () => {
+  let num1 = Number(num1Text.value);
+  let num2 = Number(num2Text.value);
+
+  resultElem.textContent = generateRandomNum(num1, num2);
+});
+
+/*
+ideas:
+center everything in a fixed position
+  possibly grid
+custom font
+color scheme
+
+maybe:
+clear button
+*/
